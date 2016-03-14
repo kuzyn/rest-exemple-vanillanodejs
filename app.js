c420d2ac09f04ac9f84803a6ec460b9e783e4763
kuzyn <hello@samuelcousin.com>
2016-03-14 05:29:59 -0400

    baseline: :penguin: :dancers: :fire:

---
+++ app.js
@@ -0,0 +1,108 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+
+  if (req.url === '/' && (req.method === 'GET' || req.method === 'POST')) {
+    // handle our only route
+
+    if (req.method === 'POST') {
+      // console.log('POST');
+      var postVarValue = '';
+
+      req.on('data', function(chunk) {
+        // listen for data & buffer it
+        postVarValue += chunk.toString();
+      });
+
+      req.on('end', function() {
+        // process the data once the transfert is done
+        postVarValue = /postVar=(.+)/.test(postVarValue) && postVarValue.match(/postVar=(.+)/)[1];
+
+        if (!!postVarValue) {
+          res.writeHead(200, "OK", {
+            'Content-Type': 'text/html'
+          });
+          res.end(createPayload(200, req, postVarValue));
+        }
+
+        res.writeHead(400, 'Bad Request', {
+          'Content-Type': 'text/html'
+        });
+        res.end(createPayload(400, req));
+
+      });
+
+    }
+
+    if (req.method === 'GET') {
+      // handle our get on /
+      res.writeHead(200, 'OK', {
+        'Content-Type': 'text/html'
+      });
+      res.end(createPayload(200, req));
+    }
+
+  } else if (req.url === '/favicon.ico') {
+    // never forget the favicon!
+
+    var favicon = new Buffer('AAABAAEAEBAAAAEAIABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAACAAAABAAAAAYAAAAJAAAADAAAAA8AAAASAAAAEwAAABYAAAAXAAEDCgAAAAEAAAAAAAAAAQAAAAEAAAACAAAABgAAAAkAAAAPAAAAFgAAACAAAAApAAAAMAAAADIAAAA3UpXF/zZwqP8AAAADAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACBC8CAD+b/0ql3P8NMWKOAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACBC8CGCOk/xcipv9PqOH/ATVhBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADXCv/x4pmP9Iq+H/Xbzr/x1wtZ4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFB6p/5ja8/9brun/Xq/s/2q27P84e7mDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEx2U/yAtrv9De8n/idTy/yRv1/9yvvD/J0xmSwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAj7jY/4XP7v9PnNv/d8fp/xgjoP8XIqr/FB+p/yRDX3EAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALYG+/3TH7P93tuX/Hi3C/5XL6v+J0u3/DxeF/57X8P9Th7yZAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADB51/yWOzv9VsN//Gyeh/x0qsP8wR7T/l9Pu/3O15v+j0u3/VoW4hgAAAAAAAAAAAAAAAAAAAAAAAAAADiaU/yBd0P8bR6v/fsjn/1Jhuf8zPaj/kdrw/2614/8YLcD/MKbd/wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABg/of9EmeP/Nobe/x5MqP8SLYb/ocPj/1W64v8PGZT/ERue/xsnqv8rPVh5AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHEmioTmBz/8xd9n/PZjj/zl8xf+Ap9H/aqTT/ztlt/9yst//RHq1swAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJ2Os3k6j3P9Xs+r/RKjn/ziN4P8eUMH/HU2y/wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAI1us/yNWtP8zddL/IFjL/xhJxv8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//8AAP/zAAD/4wAA/8cAAP+DAAD/AwAA/gcAAPwHAAD4AwAA8AMAAOAHAADgBwAA8AMAAPwHAAD/BwAA//8AAA==', 'base64');
+
+    res.writeHead(200, "OK", {
+      'Content-Type': 'image/x-icon'
+    });
+    res.end(favicon);
+
+  } else {
+    // handle routes other than / and unsupported verbs
+    res.writeHead(503, 'Not implemented', {
+      'Content-Type': 'text/html'
+    });
+    res.end(createPayload(503, req));
+
+  }
+
+});
+
+server.listen(8000);
+
+
+/////////////
+// HELPERS //
+/////////////
+
+/**
+ * Given an http request, a response code and an optional postData, generate the html markup response
+ * @param  {number} code      http status code
+ * @param  {object} request   the node http request
+ * @param  {string} postData  [optional] used to pass the postData after a 200-OK from post
+ * @return {string}           a string object with our assembled markup
+ */
+function createPayload(code, request, postData) {
+
+  var payload = '<html><head>';
+
+  if (code === 200 && !postData) {
+    payload +=  '<title>200 - OK</title></head><body><h1>200 - OK</h1><h1>Your language is: ' +
+                request.headers['accept-language'] + '</h1><h1>You sent a: ' +
+                request.method + ' to ' + request.url + '</h1>';
+  }
+
+  if (code === 200 && request.method === 'POST' && !!postData) {
+    payload +=  '<title>200 - OK</title></head><body><h1>200 - OK</h1><h1>Your language is: ' +
+                request.headers['accept-language'] + '</h1><h1>You sent a: ' +
+                request.method + ' to ' + request.url + '</h1><h1>Your POST variable value: ' + postData + '</h1>';
+  }
+
+  if (code === 400) {
+    payload +=  '<title>400 - Bad Request</title></head><body><h1>400 - Bad Request</h1><h1>You sent a: ' +
+                request.method + ' to ' + request.url + '</h1><h1>Could not process query because postVar was empty</h1>';
+  }
+
+  if (code === 503) {
+    payload +=  '<title>503 - Not Implemented</title></head><body><h1>503 - Not Implemented</h1>' +
+                '<h1>You sent a: ' + request.method + ' to ' + request.url + '</h1>';
+  }
+
+  return payload += '</body></html>';
+
+}
---
+++ readme.md
@@ -0,0 +1,73 @@
+## Description
+### :dancers: :dancers: :dancers:
+
+### nodejs REST example
+
+Super tiny RESTful API example in vanilla [nodejs](http://nodejs.org/)  
+
+One file, just over a hundred line of code, with zero dependencies, a single module require (http, duh!), get and post handling at `/`, error handling for all the rest, and a pizza favicon for good mesure.
+
+Not fit for production :scream_cat:
+
+## Deploy
+
+Well, that's a pretty basic piece of :cake:
+
+```bash
+$ node app.js
+```
+and either visit `localhost:8000` in your browser, hit it up with [Postman](https://www.getpostman.com/) or curl it like so:  
+```bash
+$ curl -i localhost:8000  
+$ curl -i --data "postVar=foo" localhost:8000  
+$ curl -i -X PUT -d "putVar=foo" localhost:8000  
+$ curl -i -X DELETE "putVar=foo" localhost:8000  
+$ curl --request PATCH localhost:8000  
+```
+*From top to bottom: `GET`, `POST`, `PUT`, `DELETE`, `PATCH`*  
+
+**`POST` queries on `/` will process only *x-www-form-urlencoded* & *raw* (i.e. `postVar=foo`) bodies**
+
+## API
+Here are the default routes:  
+
+Route | Type | Return | Payload | Description
+--- | --- | --- | --- | ---
+*/*       | **[GET]**     | Object  | n/a | Returns an HTML response
+*/*       |  **[POST]**   | Object  | {postVar: *string*} | Returns an HTML response (returns a 400 error if postVar is null)
+*/*       | **[ALL OTHER VERBS]**       | Object  | n/a | Returns an HTML error
+
+### Result examples
+Return body on success:
+```html
+<html>
+ <head><title>200 - OK</title></head>
+ <body>
+  <h1>200 - OK</h1>
+  <h1>Your language is: en-US,en;q=0.8,fr-CA;q=0.6,fr;q=0.4</h1>
+  <h1>You sent a: POST to /</h1>
+  <h1>Your POST variable value: foo</h1>
+ </body>
+</html>
+```  
+
+Return body on error:
+```html
+<html>
+ <head><title>503 - Not Implemented</title></head>
+ <body>
+  <h1>503 - Not Implemented</h1>
+  <h1>You sent a: PATCH to /</h1>
+ </body>
+</html>
+```
+
+## License
+The MIT License (MIT)
+Copyright (c) 2016 Samuel Cousin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
